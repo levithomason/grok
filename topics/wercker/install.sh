c0642725e1118c6929eec3371f842f0feefa633c
levithomason <me@levithomason.com>
2015-08-05 20:00:10 -0700

    chmod wercker cli after install

--- topics/wercker/install.sh
+++ topics/wercker/install.sh
@@ -3,4 +3,8 @@
 # Wercker CLI
 #
 # This CLI is not available on brew so we have to grab it manually
-curl -L https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker -o $GROK_BIN/wercker
+
+echo "... downloading latest stable wercker cli"
+curl -sL https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker -o $GROK_BIN/wercker
+chmod +x $GROK_BIN/wercker
+wercker version
