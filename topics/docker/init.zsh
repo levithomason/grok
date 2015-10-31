7f6950572c8aa4dee3887c0e78fcc7caf9febed5
levithomason <me@levithomason.com>
2015-10-30 23:06:18 -0700

    update boot2docker to docker-machine

--- topics/docker/init.zsh
+++ topics/docker/init.zsh
@@ -1,4 +1,4 @@
-# initialize boot2docker environment if it exists
-if type boot2docker > /dev/null 2>&1; then
-    eval `boot2docker shellinit 2>/dev/null`
+# initialize default docker environment if it exists
+if type docker-machine > /dev/null 2>&1; then
+  eval `docker-machine env default 2> /dev/null`
 fi
