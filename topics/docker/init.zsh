4eb388376887a0cc247882d009e0736dca6a4f58
levithomason <me@levithomason.com>
2016-02-12 10:06:30 -0800

    start default docker machine on shell start

--- topics/docker/init.zsh
+++ topics/docker/init.zsh
@@ -1,4 +1,5 @@
 # initialize default docker environment if it exists
 if type docker-machine > /dev/null 2>&1; then
+  docker-machine start default
   eval `docker-machine env default 2> /dev/null`
 fi
