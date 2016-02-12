fc25270b49780a9f241d9ae526fca7005d0587df
levithomason <me@levithomason.com>
2016-02-12 10:09:09 -0800

    squelch docker start output

--- topics/docker/init.zsh
+++ topics/docker/init.zsh
@@ -1,5 +1,6 @@
 # initialize default docker environment if it exists
 if type docker-machine > /dev/null 2>&1; then
-  docker-machine start default
+  echo "... starting default docker machine"
+  docker-machine start default > /dev/null 2>&1
   eval `docker-machine env default 2> /dev/null`
 fi
