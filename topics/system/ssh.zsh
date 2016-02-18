8ff1146b1e83065f381a1fce8f4d683475d2be6f
levithomason <me@levithomason.com>
2016-02-17 17:07:48 -0800

    start ssh-agent before ssh-add

--- topics/system/ssh.zsh
+++ topics/system/ssh.zsh
@@ -5,6 +5,10 @@
 
 echo "... adding ~/.ssh identities"
 
+# start agent
+eval `ssh-agent -s`
+
+# add all keys
 for key in $(ls ~/.ssh/ | grep identity | grep -v \.pub$); do
   ssh-add ~/.ssh/$key > /dev/null 2>&1
 done
