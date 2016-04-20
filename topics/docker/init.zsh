fb9f1d8278b6d12d65ce9b22f604c14af262ce42
levithomason <me@levithomason.com>
2016-04-20 16:15:44 -0700

    feat(init-docker): move from startup to func

--- topics/docker/init.zsh
+++
@@ -1,6 +0,0 @@
-# initialize default docker environment if it exists
-if type docker-machine > /dev/null 2>&1; then
-  echo "... starting default docker machine"
-  docker-machine start default > /dev/null 2>&1
-  eval `docker-machine env default 2> /dev/null`
-fi
