81255a4a0bb68587c3ce59bf60c1ca90ce31fb98
Levi Thomason <me@levithomason.com>
2016-07-15 14:34:39 -0700

    feat(npm): add aliases

---
+++ topics/npm/aliases.zsh
@@ -0,0 +1,10 @@
+alias ns='npm run start'
+alias nt='npm run test'
+alias nl='npm run lint'
+alias nb='npm run build'
+
+alias nsw='npm run start:watch'
+alias ntw='npm run test:watch'
+alias nlw='npm run lint:watch'
+alias nbw='npm run build:watch'
+
