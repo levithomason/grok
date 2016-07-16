1064a2e372662886a763ea08241b6df46871ad8e
Levi Thomason <me@levithomason.com>
2016-07-16 15:34:13 -0700

    feat(npm): always --silent

--- topics/npm/aliases.zsh
+++ topics/npm/aliases.zsh
@@ -1,10 +1,10 @@
-alias ns='npm run start'
-alias nt='npm run test'
-alias nl='npm run lint'
-alias nb='npm run build'
+alias ns='npm run start --silent'
+alias nt='npm run test --silent'
+alias nl='npm run lint --silent'
+alias nb='npm run build --silent'
 
-alias nsw='npm run start:watch'
-alias ntw='npm run test:watch'
-alias nlw='npm run lint:watch'
-alias nbw='npm run build:watch'
+alias nsw='npm run start:watch --silent'
+alias ntw='npm run test:watch --silent'
+alias nlw='npm run lint:watch --silent'
+alias nbw='npm run build:watch --silent'
 
