de2bc0a69194d1a0c159cdbbdd0d2a9db5b7ffd3
Levi Thomason <me@levithomason.com>
2016-07-22 10:33:44 -0700

    feat(npm): add i/un aliases

--- topics/npm/aliases.zsh
+++ topics/npm/aliases.zsh
@@ -1,10 +1,12 @@
-alias ns='npm run start --silent'
-alias nt='npm run test --silent'
+alias ni='npm install'
+alias nu='npm uninstall'
+alias ns='npm start --silent'
+alias nt='npm test --silent'
 alias nl='npm run lint --silent'
 alias nb='npm run build --silent'
 
-alias nsw='npm run start:watch --silent'
-alias ntw='npm run test:watch --silent'
+alias nsw='npm start:watch --silent'
+alias ntw='npm test:watch --silent'
 alias nlw='npm run lint:watch --silent'
 alias nbw='npm run build:watch --silent'
 
