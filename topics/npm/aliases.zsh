a0658d357a5138d9d5c3192b2282b06d3c8e4043
Levi Thomason <me@levithomason.com>
2016-08-05 08:22:19 -0700

    feat(npm): add lint:fix alias

--- topics/npm/aliases.zsh
+++ topics/npm/aliases.zsh
@@ -8,5 +8,6 @@ alias nb='npm run build --silent'
 alias nsw='npm run start:watch --silent'
 alias ntw='npm run test:watch --silent'
 alias nlw='npm run lint:watch --silent'
+alias nlf='npm run lint:fix --silent'
 alias nbw='npm run build:watch --silent'
 
