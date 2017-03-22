256d042f0a4bd129d15d4163b0f6acc91c7837ae
Levi Thomason <me@levithomason.com>
2017-03-21 17:25:33 -0700

    feat(un_topic_zsh_files): echo filenames

--- lib/run_topic_zsh_files.zsh
+++ lib/run_topic_zsh_files.zsh
@@ -8,6 +8,9 @@
 # 3) completion.zsh
 
 echo 'GROK: run_topic_zsh_files'
+log_file() {
+  echo "    ${1/${GROK_ROOT}\//}"
+}
 
 #
 # Unique array of all `*.zsh` files
@@ -16,13 +19,13 @@ echo 'GROK: run_topic_zsh_files'
 typeset -U config_files
 config_files=($GROK_TOPICS/**/*.zsh)
 
-
 #
 # path.zsh
 #
 
 echo '... running /topics/**/path.zsh'
 for file in ${(M)config_files:#*/path.zsh}; do
+  log_file $file
   source $file
 done
 
@@ -34,6 +37,7 @@ done
 
 echo '... running /topics/**/*.zsh'
 for file in ${${config_files:#*/path.zsh}:#*/completion.zsh}; do
+  log_file $file
   source $file
 done
 
@@ -45,6 +49,7 @@ done
 echo '... running /topics/**/completion.zsh'
 # load every completion after autocomplete loads
 for file in ${(M)config_files:#*/completion.zsh}; do
+  log_file $file
   source $file
 done
 
