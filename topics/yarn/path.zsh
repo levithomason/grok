4f00ee6d646b7d52807f2fcf6e12564f28997bcd
Levi Thomason <me@levithomason.com>
2017-07-18 10:47:18 -0700

    fix(yarn): remove PATH TODO

--- topics/yarn/path.zsh
+++ topics/yarn/path.zsh
@@ -1,5 +1,1 @@
-# TODO
-# Node version is not set by this point.  This adds:
-#   "Node version 0.12.7 is not supported, please use Node.js 4.0 or higher."
-# to the PATH.
 export PATH="$PATH:$(yarn global bin)"
