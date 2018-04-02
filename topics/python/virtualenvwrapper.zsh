84d2f9ca54f3d51fbfd41c68648072b4fe3f5737
Levi Thomason <me@levithomason.com>
2018-04-02 12:32:45 -0700

    fix(python): skip virtualenvwrapper

--- topics/python/virtualenvwrapper.zsh
+++ topics/python/virtualenvwrapper.zsh
@@ -1,4 +1,5 @@
-export VIRTUALENV_PYTHON=/usr/local/bin/python3
-export WORKON_HOME=~/Envs
-export PROJECT_HOME=~/src/
-source /usr/local/bin/virtualenvwrapper.sh
+# export VIRTUALENV_PYTHON=/usr/local/bin/python3
+# export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+# export WORKON_HOME=~/Envs
+# export PROJECT_HOME=~/src/
+# source /usr/local/bin/virtualenvwrapper.sh
