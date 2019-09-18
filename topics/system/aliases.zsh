e86cfe8e3aea35be12c5abc89d53569f1dbdc174
Levi Thomason <me@levithomason.com>
2019-09-18 14:49:29 -0700

    feat: remove gls aliases

--- topics/system/aliases.zsh
+++ topics/system/aliases.zsh
@@ -1,16 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la="gls -A --color"
-fi
-
 alias "odot"="ws $GROK_ROOT"
-alias "cd.."="cd .."
 alias ".."="cd .."
 alias big="du -sch $*"
