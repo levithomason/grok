6518a487dc8c59b4febe621325dbfb826c6bf13b
Levi Thomason <me@levithomason.com>
2022-03-28 17:38:56 -0700

    proper freeze apps/cli

--- lib/freeze_apps.zsh
+++ lib/freeze_apps.zsh
@@ -3,6 +3,17 @@
 #
 # Freeze desktop apps
 #
+command=$1
+app=$2
 
-rm $GROK_LIB/installed_apps.txt
-brew list --cask > $GROK_LIB/installed_apps.txt
+if [[ $command == "install" ]] then
+  echo $app >> $GROK_LIB/installed_apps.txt
+fi
+
+if [[ $command == "uninstall" ]] then
+  grep -x -v $app $GROK_LIB/installed_apps.txt > $GROK_LIB/tmp
+  mv $GROK_LIB/tmp $GROK_LIB/installed_apps.txt
+fi
+
+# sort and remove duplicates
+sort -uo $GROK_LIB/installed_apps.txt $GROK_LIB/installed_apps.txt
--- lib/freeze_clis.zsh
+++ lib/freeze_clis.zsh
@@ -3,6 +3,17 @@
 #
 # Freeze CLI tools
 #
+command=$1
+cli=$2
 
-rm $GROK_LIB/installed_clis.txt
-brew list --formula > $GROK_LIB/installed_clis.txt
+if [[ $command == "install" ]] then
+  echo $cli >> $GROK_LIB/installed_clis.txt
+fi
+
+if [[ $command == "uninstall" ]] then
+  grep -x -v $cli $GROK_LIB/installed_clis.txt > $GROK_LIB/tmp
+  mv $GROK_LIB/tmp $GROK_LIB/installed_clis.txt
+fi
+
+# sort and remove duplicates
+sort -uo $GROK_LIB/installed_clis.txt $GROK_LIB/installed_clis.txt
