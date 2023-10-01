b0120aab561a3539c32c625e67caff3e0d9f1c93
Levi Thomason <me@levithomason.com>
2023-10-01 09:48:54 -0700

    comment out yarn global path

--- topics/yarn/path.zsh
+++ topics/yarn/path.zsh
@@ -1,36 +1,41 @@
-assumed="/opt/homebrew/bin"
-
-export PATH="$PATH:$assumed"
-
-# yarn global bin is slow
-# async compare actual yarn bin to assumed yarn bin
-check() {
-  actual=$(yarn global bin)
-
-  if [[ $actual != $assumed ]]; then
-    export PATH="$PATH:$actual"
-
-    local color_error="1"
-    local color_warn="3"
-    local color_dim="7"
-    local color_correct="2"
-
-    echo ""
-    echo ""
-    print -P -- "%F{$color_warn}WARNING: Update assumed yarn global bin.%f"
-    echo ""
-    print -P -- "%F{$color_error}  assumed%f $assumed"
-    print -P -- "%F{$color_correct}  actual%f  $actual"
-    print -P -- "%F{$color_dim}  in      $GROK_TOPICS/yarn/path.zsh:1:9%f"
-    echo ""
-    exit
-  fi
-
-  unset assumed
-  unset actual
-  unset set_actual
-  unset -f check
-}
-
-check &! # background
-
+# Heads Up!
+#
+# Yarn 2.x no longer has `yarn global`. Leaving this here for now just in case.
+#
+# =============================================================================
+#
+# assumed="/opt/homebrew/bin"
+#
+# export PATH="$PATH:$assumed"
+#
+# # yarn global bin is slow
+# # async compare actual yarn bin to assumed yarn bin
+# check() {
+#   actual=$(yarn global bin)
+#
+#   if [[ $actual != $assumed ]]; then
+#     export PATH="$PATH:$actual"
+#
+#     local color_error="1"
+#     local color_warn="3"
+#     local color_dim="7"
+#     local color_correct="2"
+#
+#     echo ""
+#     echo ""
+#     print -P -- "%F{$color_warn}WARNING: Update assumed yarn global bin.%f"
+#     echo ""
+#     print -P -- "%F{$color_error}  assumed%f $assumed"
+#     print -P -- "%F{$color_correct}  actual%f  $actual"
+#     print -P -- "%F{$color_dim}  in      $GROK_TOPICS/yarn/path.zsh:1:9%f"
+#     echo ""
+#     exit
+#   fi
+#
+#   unset assumed
+#   unset actual
+#   unset set_actual
+#   unset -f check
+# }
+#
+# check &! # background
