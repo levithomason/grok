e0d0dc019c02a01ab0a246bc5b3c0ef22b7432eb
Levi Thomason <me@levithomason.com>
2026-03-04 18:41:17 -0800

    add aliases for claude, comment out gls, remove rubber ducky topic as it places itself in the zshrc no matter what

---
+++ topics/claude/aliases.zsh
@@ -0,0 +1,7 @@
+# work
+alias claude="agency claude"
+alias c="claude"
+
+# personal
+alias personal-claude="/Users/levithomason/.claude-cli/currentVersion/claude"
+alias pc="personal-claude"
--- topics/rubber-ducky/path.zsh
+++
@@ -1,3 +0,0 @@
-if [[ ":${PATH}:" != *":/Users/levithomason/.config/RubberDucky/CurrentVersion:"* ]]; then
-    export PATH="/Users/levithomason/.config/RubberDucky/CurrentVersion:${PATH}"
-fi
